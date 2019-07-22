06b2145b7ee541963a29380ccf6f4c31fe18837d
yedongyang <yedongyang@ebupt.com>
2019-07-22 16:29:41 +0800

    创建项目

---
+++ src/main/java/com/yezi/springforstudy/SpringforstudyApplication.java
@@ -0,0 +1,13 @@
+package com.yezi.springforstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringforstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringforstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yezi/springforstudy/SpringforstudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yezi.springforstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringforstudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
